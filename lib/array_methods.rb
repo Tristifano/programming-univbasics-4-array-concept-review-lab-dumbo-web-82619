2fe9735f6cbfb6c9ccea443620ee6c12a8f6cf8e
Tristan Myers <tristifano@gmail.com>
2019-08-04 14:21:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,6 @@ def find_element_index(array, value_to_find)
   for index in 0...array.length
     if array[index] == value_to_find
       return index
-    else
-      return nil
     end
   end
 end
